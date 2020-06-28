a01c2d67716deed4443f650e8b71fe8885204640
freezingcarbon <freezing.carbon@gmail.com>
2020-06-28 07:11:46 +0700

    first commit

---
+++ FastConnect-Sample/Sample.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using G.Libbrary.FastConnect;
+using System;
+using System.Linq;
+
+namespace FastConnect_Sample
+{
+    public class Sample
+    {
+        public void test01()
+        {
+            var db = (new OracleDB("TestEnv")).GetConnection();
+
+            var data_query = db.Query<PO>("select top 10 *, abc as test001 from x where id = @index", new { index = 10 }).ToArray();
+            var data_store = db.Query<PO>("store_procedure_name", new { po_no = "000001" }, commandType: System.Data.CommandType.StoredProcedure);
+        }
+    }
+
+    public class PO
+    {
+        public string po_number { get; set; }
+        public string barcode { get; set; }
+        public string test001 { get; set; }
+    }
+
+    /* example store procedure
+     store procedure (
+     @po_no varchar(20)
+     )
+     begin
+     select * from x where po_no = @po_no
+     end
+     */
+}
